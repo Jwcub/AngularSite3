57c577a8bab7a579bbccd4026147c9ca0a54a0d5
Kevin.M <kema2501@student.miun.se>
2026-05-02 22:01:25 +0200

    Lägg till funktion för att visa vilken kolumn som sorteras

--- src/app/schedule/schedule.ts
+++ src/app/schedule/schedule.ts
@@ -1,7 +1,6 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { CourseService } from '../services/course.service';
 import { Course } from '../models/course.interface';
-import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-schedule',
@@ -30,7 +29,7 @@ export class Schedule {
 
   // Filtera kurser med sökfras
   filteredCourses = computed(() => {
-    const searchTerm = this.searchQuery().toLowerCase();//Filter triggered during change of search item
+    const searchTerm = this.searchQuery().toLowerCase();
     let filteredList = this.courseList().filter((course) => 
       course.coursename.toLowerCase().includes(searchTerm) ||
       course.code.toLowerCase().includes(searchTerm));
@@ -45,21 +44,25 @@ export class Schedule {
       this.error.set("Något gick fel...")
     }
   }
+  
+  arrow = signal<string>("");
 
   // Sortera kurskod i bokstavsordning
   sortCourseCode() {
     this.filteredCourses().sort((a, b) => a.code.localeCompare(b.code));
+    this.arrow.set("code");
   }
 
   // Sortera kurskod i bokstavsordning
   sortCourseName() {
     this.filteredCourses().sort((a, b) => a.coursename.localeCompare(b.coursename));
+    this.arrow.set("name");
   }
 
   // Sortera kurskod i bokstavsordning
   sortCourseProgression() {
     this.filteredCourses().sort((a, b) => a.progression.localeCompare(b.progression));
+    this.arrow.set("progression");
   }
 }
-
 
